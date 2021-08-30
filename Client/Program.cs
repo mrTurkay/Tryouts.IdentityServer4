55f1929875f24967d31fd92d8a6b2c96cfba5b95
Murat <murat@mdasocial.com>
2021-08-30 16:35:13 +0300

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,71 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var disco = await Discover(client);
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+            //var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            //{
+            //    Address = disco.TokenEndpoint,
+
+            //    ClientId = "client2",
+            //    ClientSecret = "secret2",
+            //    Scope = "api2"
+            //});
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:6001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+
+        }
+
+        public static async Task<DiscoveryDocumentResponse> Discover(HttpClient client)
+        {
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                throw new Exception("disco error" + disco.Error);
+            }
+
+            return disco;
+        }
+    }
+}
